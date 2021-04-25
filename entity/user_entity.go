39d5e0587825d8f98aba63b8565b1060803de479
ryu7 <ryu@jeneusindonesia.com>
2021-04-25 13:39:40 +0700

    change response

--- entity/user_entity.go
+++ entity/user_entity.go
@@ -3,14 +3,14 @@ package entity
 import "time"
 
 type User struct {
-	ID           uint      `gorm:"column:id"`
-	UserName     string    `gorm:"column:user_name"`
-	Phone        string    `gorm:"column:phone"`
-	Email        string    `gorm:"column:email"`
-	PasswordHash string    `gorm:"password_hash"`
-	RegisteredAt time.Time `gorm:"column:registered_at"`
-	LastLogin    time.Time `gorm:"column:last_login"`
-	Profile      string    `gorm:"column:profile"`
+	ID           uint      `gorm:"column:id" json:"id"`
+	UserName     string    `gorm:"column:user_name" json:"username"`
+	Phone        string    `gorm:"column:phone" json:"phone"`
+	Email        string    `gorm:"column:email" json:"email"`
+	PasswordHash string    `gorm:"password_hash" json:"-"`
+	RegisteredAt time.Time `gorm:"column:registered_at" json:"register_at"`
+	LastLogin    time.Time `gorm:"column:last_login" json:"last_login"`
+	Profile      string    `gorm:"column:profile" json:"profile"`
 }
 
 func (User) TableName() string {
